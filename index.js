1abfbdf693d9145132d72c1d5b1c1a70118e6b28
chief <mahdirashidi462@gmail.com>
2023-09-05 22:07:10 +0330

    fix add all modules in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+require('./src/js/addNewFile');
+require('./src/js/createFile');
+require('./src/js/addNewContact');
+// require('./src/js/yargs.config');
+const yargs = require('yargs');
+yargs.command({
+    command: 'create',
+    describe: '[create new contact]',
+    'builder': {
+        fullName: {
+            'command': 'fullname',
+            'describe': '[contact fullname]',
+            'alias': 'f',
+            demandOption: true,
+            type: 'string'
+        },
+        'email': {
+
+        },
+        'fullName': {
+
+        },
+    }
+})
+console.log(yargs.argv)
